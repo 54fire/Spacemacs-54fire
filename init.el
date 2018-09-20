6d2d80fc01d031013ab821201c1e9e6c16276a3b
54fire <15123375513@163.com>
2018-09-20 15:43:35 +0800

    我也不知道改了什么

--- init.el
+++ init.el
@@ -31,6 +31,7 @@ values."
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
    '(
+     python
      ;; ----------------------------------------------------------------
      ;; Example of useful layers you may want to use right away.
      ;; Uncomment some layer names and press <SPC f e R> (Vim style) or
@@ -369,26 +370,25 @@ you should place your code here."
 
       (setq w3m-use-cookies t)
 
-      (setq w3m-command-arguments '("-cookie" "-F"))               
-
-      (setq browse-url-browser-function 'w3m-browse-url)                
+      (setq w3m-command-arguments '("-cookie" "-F"))
+      (setq browse-url-browser-function 'w3m-browse-url)
       (setq w3m-view-this-url-new-session-in-background t)
 
 
-      (setq w3m-show-graphic-icons-in-header-line t)                  
-      (setq w3m-show-graphic-icons-in-mode-line t) 
+      (setq w3m-show-graphic-icons-in-header-line t)
+      (setq w3m-show-graphic-icons-in-mode-line t)
 
-      (setq w3m-view-this-url-new-session-in-background t)  
+      (setq w3m-view-this-url-new-session-in-background t)
 
-      (add-hook 'w3m-fontify-after-hook 'remove-w3m-output-garbages)                                    
-      (defun remove-w3m-output-garbages ()                            
-        "去掉w3m输出的垃圾."                                            
-        (interactive)                                                  
-        (let ((buffer-read-only))                                       
-          (setf (point) (point-min))                                      
-          (while (re-search-forward "[\200-\240]" nil t)                  
-            (replace-match " "))                                            
-          (set-buffer-multibyte t))                                       
+      (add-hook 'w3m-fontify-after-hook 'remove-w3m-output-garbages)
+      (defun remove-w3m-output-garbages ()
+        "去掉w3m输出的垃圾."
+        (interactive)
+        (let ((buffer-read-only))
+          (setf (point) (point-min))
+          (while (re-search-forward "[\200-\240]" nil t)
+            (replace-match " "))
+          (set-buffer-multibyte t))
         (set-buffer-modified-p nil))
       (setq w3m-home-page "https://www.baidu.com")
 
@@ -401,7 +401,7 @@ you should place your code here."
         (regexp-opt '(".ogg" ".mp3" ".wav" ".mpg" ".mpeg" ".wmv" ".wma"
                       ".mov" ".avi" ".divx" ".ogm" ".asf" ".mkv" "http://" "mms://"
                       ".rm" ".rmvb" ".mp4" ".flac" ".vob" ".m4a" ".flv" ".ogv" ".pls"))
-        "mplayer" "-slave" "-quiet" "-really-quiet" "-fullscreen") 
+        "mplayer" "-slave" "-quiet" "-really-quiet" "-fullscreen")
 )
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
